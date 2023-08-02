6bdb937729d916bbbd3dbe4dd275c2d90a1e0bff
sudestec <facu.vdp@gmail.com>
2023-08-02 05:16:55 -0300

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  output: 'hybrid',
+  output: 'server',
   adapter: cloudflare({ mode: 'directory' })
 });
